a3dd40ee39cdb9e58620dbd6699ca73e94a7086a
ftjuanxd <davidstivenpinuela@gmail.com>
2025-07-15 14:43:47 -0500

    Fix: Normalize gradlew line endings to LF

--- app/src/main/java/com/zonedev/minapp/ui/theme/Screen/Guardia/Element.kt
+++
@@ -1,9 +0,0 @@
-package com.zonedev.minapp.ui.theme.Screen.Guardia
-
-import androidx.compose.runtime.Composable
-import com.zonedev.minapp.ui.theme.Templates.Template_Text
-
-@Composable
-fun Element(guardiaId: String) {
-    Template_Text( guardiaId = guardiaId)
-}
--- app/src/main/java/com/zonedev/minapp/ui/theme/Screen/Report.kt
+++
@@ -1,9 +0,0 @@
-package com.zonedev.minapp.ui.theme.Screen
-
-import androidx.compose.runtime.Composable
-import com.zonedev.minapp.ui.theme.Components.Report.DropdownMenu
-
-@Composable
-fun ScreenReport(guardiaId: String){
-    DropdownMenu(guardiaId)
-}
